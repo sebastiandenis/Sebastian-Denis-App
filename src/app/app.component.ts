3c24df81b68c6f0844298769637f61f5bad29588
Sebastian Denis <sebastian.denis@outlook.com>
2019-01-09 23:24:13 +0100

    pl translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { TranslateService } from "@ngx-translate/core";
 export class AppComponent {
   constructor(translate: TranslateService) {
      // this language will be used as a fallback when a translation isn't found in the current language
-     translate.setDefaultLang("en");
+     translate.setDefaultLang("pl");
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
-    translate.use("en");
+    translate.use("pl");
   }
 }
